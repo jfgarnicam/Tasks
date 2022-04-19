b46a14f386088044a64809892f7e193eab70865f
Fernando <jfgarnicam@unal.edu.co>
2022-04-19 16:35:10 +0200

    Pseudocode Task1

--- Program.cs
+++ Program.cs
@@ -4,7 +4,7 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Interview Task 1");
+            Console.WriteLine("Interview Task 2");
             //INPUTS = list of points and values plane equation [(x,y,z), ....] and [a,b,c,d]
             //OUTPUTS = Points over the plane and intersection points and POLYLINE!
 
@@ -15,6 +15,18 @@
             // 4. Return Polyline + Points above and over the plane!
 
             // Test cases: Plane z=0, vertical plane and diagonal plane
+
+            Console.WriteLine("Interview Task 1");
+            //INPUTS = A string with whitespaces a sign +or- and digits
+            //OUTPUTS = The integer number after the +or- symbol
+
+            //Pseudocode
+            // 1. Iterate ober the string to detect the +or- symbol to activate a flag
+            // 2. Save next character in a list
+            // 3. Continue itereting until end or caracter is not an integer
+            // 4. CAsting to integer and return
+
+            // Test cases: "", "     ", "    +38",  "    +c", "    +38c7",
         }
     }
 }
