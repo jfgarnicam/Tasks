d694ba88c6a1a0d906e1267015a12de5fcb62a50
Fernando <jfgarnicam@unal.edu.co>
2022-04-19 16:03:43 +0200

    Pseudocode Task2

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿namespace InterviewTasks
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Interview Task 1");
+            //INPUTS = list of points and values plane equation [(x,y,z), ....] and [a,b,c,d]
+            //OUTPUTS = Points over the plane and intersection points and POLYLINE!
+
+            //Pseudocode
+            // 1. Detect points above the plane
+            // 2. Detect pair of points in oposite sites of the plane
+            // 3. Calculate the intersection point
+            // 4. Return Polyline + Points above and over the plane!
+        }
+    }
+}
